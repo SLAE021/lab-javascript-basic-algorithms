00b6a334c9f305107d5495db0c126064decd740e
Samuel Angulo <samuelangulo70@gmail.com>
2024-08-28 16:52:24 +0200

    primera parte de laboratorio

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // Iteration 1: Names and Input
+ /*1.1 Crear una variable hacker1 con el nombre del conductor.*/
 
+let hacker1= "Samuel"
+
+console.log(`The driver's name is ${hacker1}`)
+
+let hacker2="Angela"
+let hacker3="Sofia"
+
+console.log(`The navigator's name is ${hacker2} y ${hacker3}`)
 
 // Iteration 2: Conditionals
 
