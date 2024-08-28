515d336079937fb53db8f6936d46a349abe38cfc
Angela Ruiz <angela.ruiz.rodriguez.8@gmail.com>
2024-08-28 19:12:12 +0200

    Solved lab

--- index.js
+++ index.js
@@ -11,21 +11,18 @@ let hacker3="Sofia"
 console.log(`The navigator's name is ${hacker2} y ${hacker3}`)
 
 // Iteration 2: Conditionals
- 
- if ((hacker1.length>hacker2.length) && (hacker1.length>hacker3.length)){
+
+if (hacker1.length > hacker2.length) {
     console.log (`The driver has the longest name, it has ${hacker1.length} characters.`)
 }
-else if ((hacker2.length > hacker1.length) && (hacker2.length>hacker3.length)){
+else if (hacker2.length > hacker1.length) {
     console.log (`It seems that the navigator one has the longest name, it has ${hacker2.length} characters.`)
 }
-else if ((hacker3.length > hacker1.length) && (hacker3.length>hacker2.length)){
-    console.log (`It seems that the navigator two has the longest name, it has ${hacker3.length} characters.`)
-}
-else if ((hacker1.length===hacker2.length)&& (hacker1.length===hacker3.length)){
+else {
     console.log (`Wow, you both have equally long names, ${hacker1.length} characters!.`)
 }
 
- 
+
  // Iteration 3: Loops
 
  let driver = "";
